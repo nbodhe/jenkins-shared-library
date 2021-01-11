740c644c579d4f7bee071e496a85affe43c7384b
nbodhe <bodhe.nikhil@gmail.com>
2021-01-11 18:41:44 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -8,7 +8,7 @@ def call() {
                 }
             }
             stage('Build'){
-                stpes{
+                steps{
                     bat 'echo "Build"'
                 }
             }
